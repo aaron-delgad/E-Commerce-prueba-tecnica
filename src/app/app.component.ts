58ce02735d4af9f7e098aa96bfbefa9a7cdf115d
Aaron <radees.24.16@gmail.com>
2022-06-01 15:59:35 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'com-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Ecommerce';
+}
