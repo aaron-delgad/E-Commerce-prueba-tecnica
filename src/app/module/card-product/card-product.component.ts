7070986b51a18f79ce0bbd42fe2c7556c456a57f
Aaron <radees.24.16@gmail.com>
2022-06-02 11:28:47 -0500

    uno

--- src/app/module/card-product/card-product.component.ts
+++ src/app/module/card-product/card-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from "../../shared/service/cart.service";
 import {Product} from "../../shared/model/base/product";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'com-card-product',
@@ -9,15 +10,27 @@ import {Product} from "../../shared/model/base/product";
 })
 export class CardProductComponent implements OnInit {
 
-  cartProducts: Product[] = [];
+  cartProducts: Product[] = [
+    {}
+  ];
+  form: FormGroup;
 
-  constructor(private readonly cartService: CartService) { }
+  constructor(private readonly cartService: CartService) { this.formBuild();}
 
   ngOnInit(): void {
     this.cartService.myCart$.subscribe(produ => {
       this.cartProducts = produ;
       console.log(this.cartProducts);
     });
+  }
+
+  formBuild(){
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      subtotal: new FormControl('', [Validators.required]),
+      send: new FormControl('', [Validators.required]),
+      total: new FormControl('', [Validators.required])
+    })
   }
 
 }
--- src/app/module/card-product/card-product.module.ts
+++ src/app/module/card-product/card-product.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import {MatListModule} from '@angular/material/list';
 import { CardProductRouting } from './card-product.routing';
 import {CardProductComponent} from "./card-product.component";
-
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,12 @@ import {CardProductComponent} from "./card-product.component";
   imports: [
     CommonModule,
     CardProductRouting,
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class CardProductModule { }
